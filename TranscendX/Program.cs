d3bcaa8772928d1715ec94e8c605887b564af9b9
AustinRGovender <austinryang1@gmail.com>
2020-01-16 23:13:51 +0200

    initial

--- TranscendX/Program.cs
+++ TranscendX/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace TranscendX
 {
@@ -10,6 +14,39 @@ namespace TranscendX
     {
         static void Main(string[] args)
         {
+            ValidateFile();
+        }
+
+        
+
+        
+
+        static void ValidateFile()
+        {
+            var path = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", path + "\\input.xsd");
+            XmlReader rd = XmlReader.Create(path + "\\input.xml");
+            XDocument doc = XDocument.Load(rd);
+            doc.Validate(schema, ValidationEventHandler);
+            Console.WriteLine("Success");
+            Console.ReadLine();
+        }
+
+        static void TransformFile()
+        {
+
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+
+            }
+
         }
     }
 }
